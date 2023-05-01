b9998900dc6bead4b17fc7b42684d189236fa338
ajaychirme <ajaychirme84@gmail.com>
2023-05-01 09:37:52 +0530

    change6

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ import { fileURLToPath } from "url";
 import authRoutes from "./routes/auth.js";
 import userRoutes from "./routes/users.js";
 import postRoutes from "./routes/posts.js";
-import { register } from "./controllers/auth.js";
+ import { register } from "./controllers/auth.js";
 import {createPost} from "./controllers/posts.js"
 import { verifyToken } from "./middleware/auth.js";
 import User from "./models/User.js";
@@ -50,7 +50,7 @@ res.send("<h1>APIs</h1>"
   )
 })
 
-app.post("/auth/register", upload.single("picture"),register)
+app.post("/register", upload.single("picture"),register)
 app.post("/posts",verifyToken, upload.single("picture"), createPost);
 
 /* ROUTES*/
