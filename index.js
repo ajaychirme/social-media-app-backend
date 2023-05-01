ba2b1dd689d9de8cb75828f87698a5594c7269f9
ajaychirme <ajaychirme84@gmail.com>
2023-05-01 17:24:52 +0530

    msg3

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ res.send("<h1>APIs</h1>"
   )
 })
 
-app.post("/register", upload.single("picture"),register)
+app.post("/auth/register", upload.single("picture"),register)
 app.post("/posts",verifyToken, upload.single("picture"), createPost);
 
 /* ROUTES*/
