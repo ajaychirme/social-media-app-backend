7757e69f4c3a0f65161a612abf9acf9c11c2aa6b
ajaychirme <ajaychirme84@gmail.com>
2023-05-01 12:06:32 +0530

    corschanges5

--- routes/posts.js
+++ routes/posts.js
@@ -9,7 +9,7 @@ router.get("/", verifyToken, getFeedPosts);
 router.get("/:userId/posts", verifyToken, getUserPosts);
 
 /* UPDATE */
-router.patch("/:id/like", verifyToken, likePost);
+router.patch("/like/:id", verifyToken, likePost);
 router.patch("/:id/comment", verifyToken ,commentPost);
 
 export default router;
