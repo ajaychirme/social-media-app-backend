095b0338bc0d0d28b36e4c7a96f5974ac2cc22b9
ajaychirme <ajaychirme84@gmail.com>
2023-05-01 11:58:32 +0530

    corschanges5

--- index.js
+++ index.js
@@ -24,12 +24,13 @@ const __dirname = path.dirname(__filename);
 dotenv.config();
 const app = express();
 app.use(express.json());
+app.use(cors());
 app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors());
+
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
 //file storage :: If anyone uploads file it will store in following destination
